dc4428c7c79d40ebb53e90c8f2c2a4036bdb80e3
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-17 17:23:31 -0500

    feat: first commit

---
+++ src/main/java/ContactManagerApplication.java
@@ -0,0 +1,5 @@
+public class ContactManagerApplication {
+    public static void main(String[] args) {
+        System.out.println("Contacts manager application");
+    }
+}
---
+++ src/test/java/ContactTest.java
@@ -0,0 +1,2 @@
+public class ContactTest {
+}
